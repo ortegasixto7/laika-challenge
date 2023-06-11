b458d6ad61163e2ae60ef4f43dfa7388ed73bdf7
Sixto Ortega <ortegasixto7@gmail.com>
2023-06-11 14:25:28 -0400

    Adding movie and persistence file

---
+++ src/core/movie/IMoviePersistence.ts
@@ -0,0 +1,9 @@
+import { Movie } from './Movie'
+export interface IMoviePersistence {
+  getAll(): Promise<Movie[]>
+  getByTitleOrNull(title: string): Promise<Movie | null>
+  getByIdOrException(id: string): Promise<Movie>
+  delete(id: string): Promise<void>
+  update(data: Movie): Promise<void>
+  create(data: Movie): Promise<void>
+}
---
+++ src/core/movie/Movie.ts
@@ -0,0 +1,23 @@
+import crypto from 'crypto'
+export class Movie {
+  id: string = crypto.randomUUID()
+  title: string = ''
+  year: number = 0
+  rated: string = ''
+  released: string = ''
+  duration: string = ''
+  plot: string = ''
+  poster: string = ''
+  genres: string[] = []
+  directors: string[] = []
+  writers: string[] = []
+  actors: string[] = []
+  languages: string[] = []
+  awards: string[] = []
+  ratings: MovieRating[] = []
+}
+
+export class MovieRating {
+  source: string
+  value: string
+}
