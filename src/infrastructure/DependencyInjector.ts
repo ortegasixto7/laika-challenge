ac5fc68657b6bcca87e2ee511d391e5bc115f170
Sixto Ortega <ortegasixto7@gmail.com>
2023-06-12 22:51:43 -0400

    Adding DI for hash service and auth persistence

--- src/infrastructure/DependencyInjector.ts
+++ src/infrastructure/DependencyInjector.ts
@@ -1,6 +1,8 @@
 // Persistences
 import { IMoviePersistence } from '../core/movie/IMoviePersistence'
 import { MySQLMoviePersistence } from '../persistence/mysql/MySQLMoviePersistence'
+import { IAuthPersistence } from '../external/auth/IAuthPersistence'
+import { MySQLAuthPersistence } from '../persistence/mysql/MySQLAuthPersistence'
 
 // Services
 import { IHttpService } from '../external/http/IHttpService'
@@ -9,11 +11,15 @@ import { IMovieExternalService } from '../external/movie/IMovieExternalService'
 import { OMDBMovieExternalService } from '../external/movie/OMDBMovieExternalService'
 import { ICacheService } from '../external/cache/ICacheService'
 import { NodeCacheService } from '../external/cache/NodeCacheService'
+import { IHashService } from '../external/hash/IHashService'
+import { BcryptHashService } from '../external/hash/BcryptHashService'
 
 // Persistences
 export const moviePersistence = ((): IMoviePersistence => new MySQLMoviePersistence())()
+export const authPersistence = ((): IAuthPersistence => new MySQLAuthPersistence())()
 
 // Services
 export const httpService = ((): IHttpService => new AxiosHttpService())()
 export const movieExternalService = ((): IMovieExternalService => new OMDBMovieExternalService())()
 export const cacheService = ((): ICacheService => NodeCacheService.getInstance())()
+export const hashService = ((): IHashService => new BcryptHashService())()
